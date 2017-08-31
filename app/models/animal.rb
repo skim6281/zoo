e67edcbcac5a3a21e30a77e6a115aa61321ae408
Sam <skim6281@gmail.com>
2017-08-31 15:12:13 -0400

    Add name uniqueness validation

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,4 @@
 class Animal < ApplicationRecord
   # TODO: create validation to only allow one animal of the same name.
-  validates_uniqueness_of :name, case_sensitive: false
+  validates :name, uniqueness: { case_sensitive: false }
 end
